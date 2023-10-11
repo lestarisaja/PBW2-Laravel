ec4700e6322a4bf0d4cfa6582655c269e9c96237
lestarisaja <114651510+lestarisaja@users.noreply.github.com>
2023-10-11 19:37:07 +0700

    Add files via upload

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,67 @@
+<?php
+/*NAMA : LESTARI
+KELAS: D3IF 46-03
+NIM  : 6706223114 */
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => ['required', 'string', 'max:100','unique:users'],
+            'fullname' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'address' => ['required', 'string', 'max:1000'],
+            'birthdate' => ['required', 'date'],
+            'phoneNumber' => ['required', 'string', 'max:20'],
+            'agama' => ['required', 'string', 'max:20'],
+            'gender' => ['required', 'integer', 'max:4'],
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'fullname' => $request->fullname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'birthdate' => $request->birthdate,
+            'phoneNumber' => $request->phoneNumber,
+            'agama' => $request->agama,
+            'gender' => $request->gender,
+        ]);
+
+        event(new Registered($user));
+
+        // Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/*NAMA : LESTARI
+KELAS: D3IF 46-03
+NIM  : 6706223114 */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
